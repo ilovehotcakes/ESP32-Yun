592206dd981a4adb7c696c242b7fd4e4b903b859
Jason Chen <kidultimate@gmail.com>
2022-06-04 18:42:26 -0700

    Reorganized and renamed

--- include/motor.h
+++ include/motor.h
@@ -7,7 +7,8 @@
 #include <TMCStepper.h>
 #include <AccelStepper.h>
 #include <Preferences.h>
-#include "cover_settings.h"
+#include <PubSubClient.h>
+#include "motor_settings.h"
 
 static TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDR);
 static AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
@@ -17,15 +18,15 @@ class Motor {
 private:
   int motor = MOTOR_DISABLE;
 
-  uint32_t max_steps;
-  uint32_t current_position;
-  uint32_t previous_position = 0;
+  uint32_t maxPos;
+  uint32_t currPos;
+  uint32_t prevPos = 0;
   bool set_max = false;
   bool set_min = false;
 
+  void loadPositions();
   void moveTo(int);
   void updatePosition();
-  void load_preference();
   int percentToSteps(int);
   int stepsToPercent(int);
 
