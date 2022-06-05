2297f207a8684f7f42b80049dc4120d098b26af3
Jason Chen <kidultimate@gmail.com>
2022-06-04 21:32:59 -0700

    Reorganized the folders; added secrets_example.h

---
+++ include/secrets_example.h
@@ -0,0 +1,22 @@
+/*
+ secrets_example.h - A example of a secret.h file
+  Author: Jason Chen
+ 
+ For security reaons, WiFi and MQTT credentials are in the actual secrets.h
+ file which isn't uploaded onto GitHub. It is also a place that is easy to
+ fill in all the credentials at once instead of digging through main.
+
+ To use this file:
+  - Rename the file to "secrets.h"
+  - Uncomment the relevant fields and fillout the information
+*/
+
+// const String secretSSID       = "Your WiFi Name";
+// const String secretPass       = "Your WiFi password";
+// const String secretMqttID     = "MQTT ID for this object";
+// const String secretMqttUser   = "Your MQTT server username (Optional)";
+// const String secretMqttPass   = "Your MQTT server password (Optional)";
+// const String secretBrokerIP   = "Your MQTT server IP";
+// const int    secretBrokerPort = 1883;  // The MQTT server port is usually open on port 1883
+// const String secretInTopic    = "/server/shades/1";
+// const String secretOutTopic   = "/client/shades/1";
