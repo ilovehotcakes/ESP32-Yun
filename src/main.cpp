4be74cdcfa8e10d67ecb4076fd958c9ec75a9d02
Jason Chen <kidultimate@gmail.com>
2023-11-30 16:26:52 -0800

    MVP

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 #include <PubSubClient.h> // Mqtt
 // #define DONTCOMPILELOGS
 #include "logger.h"
-#include "motor.h"
+#include "motor_task.h"
 #if __has_include("ota.h")
 #include "ota.h"
 #endif
@@ -25,62 +25,65 @@
 #include "peripheral_connections.h"
 
 
-// States for the state machine
-enum CoverState {
-    // INITIALIZING,
-    RECONNECTING_WIFI,
-    CONNECTING_MQTT,
-    READ_MQTT_MSG
-};
+// // States for the state machine
+// enum CoverState {
+//     // INITIALIZING,
+//     RECONNECTING_WIFI,
+//     CONNECTING_MQTT,
+//     READ_MQTT_MSG
+// };
 
-// Commands recieved from MQTT
-enum Command { COVER_STOP=-1, COVER_OPEN=-2, COVER_CLOSE=-3, COVER_SET_MIN=-4, COVER_SET_MAX=-5, SYS_RESET=-98, SYS_REBOOT=-99 };
-
-
-void core0Task(void * parameter);
-void startWifi();
-void connectWifi(WiFiEvent_t event, WiFiEventInfo_t info);
-void connectMqtt();
-void sendMqtt(String);
+// // Commands recieved from MQTT
+// enum Command { COVER_STOP=-1, COVER_OPEN=-2, COVER_CLOSE=-3, COVER_SET_MIN=-4, COVER_SET_MAX=-5, SYS_RESET=-98, SYS_REBOOT=-99 };
 
 
-TaskHandle_t C0;  // Task for core 0
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-CoverState state;                      // State machine to manage WiFi/MQTT
-String   ssid       = secretSSID;      // SSID (name) for WiFi
-String   pass       = secretPass;      // Network password for WiFi
-String   mqttID     = secretMqttID;    // MQTT ID for PubSubClient
-String   mqttUser   = secretMqttUser;  // MQTT server username (optional)
-String   mqttPass   = secretMqttPass;  // MQTT server password (optional)
-String   brokerIP   = secretBrokerIP;  // IP of MQTT server
-uint16_t brokerPort = secretBrokerPort;
-String   inTopic    = secretInTopic;   // MQTT inbound topic
-String   outTopic   = secretOutTopic;  // MQTT outbound topic
+// void core0Task(void * parameter);
+// void startWifi();
+// void connectWifi(WiFiEvent_t event, WiFiEventInfo_t info);
+// void connectMqtt();
+// void sendMqtt(String);
 
-static Motor motor;
+
+// TaskHandle_t C0;  // Task for core 0
+// WiFiClient wifiClient;
+// PubSubClient mqttClient(wifiClient);
+// CoverState state;                      // State machine to manage WiFi/MQTT
+// String   ssid       = secretSSID;      // SSID (name) for WiFi
+// String   pass       = secretPass;      // Network password for WiFi
+// String   mqttID     = secretMqttID;    // MQTT ID for PubSubClient
+// String   mqttUser   = secretMqttUser;  // MQTT server username (optional)
+// String   mqttPass   = secretMqttPass;  // MQTT server password (optional)
+// String   brokerIP   = secretBrokerIP;  // IP of MQTT server
+// uint16_t brokerPort = secretBrokerPort;
+// String   inTopic    = secretInTopic;   // MQTT inbound topic
+// String   outTopic   = secretOutTopic;  // MQTT outbound topic
+
+static MotorTask motor_task(1);
 
 void setup() {
     // Start log
     LOG_INIT(9600, LogLevel::INFO);
 
     // Initialized and turn on LED to indicate boot up
+    LOGI("Booting");
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, HIGH);
 
-    motor.init();
+    motor_task.init();
+    digitalWrite(LED_PIN, LOW);
+    LOGI("Booting completed");
 
-    // Core 0 setup for dual core operation
-    disableCore0WDT();  // Disable watchdog timer
-    xTaskCreatePinnedToCore(core0Task, "Core_0", 8192, NULL, 1, &C0, 0);
+    // // Core 0 setup for dual core operation
+    // disableCore0WDT();  // Disable watchdog timer
+    // xTaskCreatePinnedToCore(core0Task, "Core_0", 8192, NULL, 1, &C0, 0);
 
-    // Start the WiFi connection
-    WiFi.onEvent(connectWifi, SYSTEM_EVENT_STA_DISCONNECTED);
-    startWifi();
+    // // Start the WiFi connection
+    // WiFi.onEvent(connectWifi, SYSTEM_EVENT_STA_DISCONNECTED);
+    // startWifi();
 
-    #if __has_include("ota.h")
-    otaSetup();
-    #endif
+    // #if __has_include("ota.h")
+    // otaSetup();
+    // #endif
 }
 
 
@@ -89,119 +92,119 @@ void setup() {
     smoothly on core 0.
 **/
 void loop() {
-    motor.run();
+    // motor.run();
 }
 
 
-/**
-    The state machine that tracks communication to MQTT/WiFi. I have tried to
-    start the WiFi in the state machine but it doesn't work. Also, the delay at
-    the of the function is required, else WiFi can't connect for unknown reason.
-**/
-void core0Task(void * parameter) {
-    for (;;) {
-        switch (state) {
-            case RECONNECTING_WIFI:
-                // Turn on LED to indicate disconnected
-                digitalWrite(LED_PIN, HIGH);
+// /**
+//     The state machine that tracks communication to MQTT/WiFi. I have tried to
+//     start the WiFi in the state machine but it doesn't work. Also, the delay at
+//     the of the function is required, else WiFi can't connect for unknown reason.
+// **/
+// void core0Task(void * parameter) {
+//     for (;;) {
+//         switch (state) {
+//             case RECONNECTING_WIFI:
+//                 // Turn on LED to indicate disconnected
+//                 digitalWrite(LED_PIN, HIGH);
 
-                // Wait for reconnection
-                if (WiFi.status() == WL_CONNECTED)
-                    state = CONNECTING_MQTT;
-            break;
+//                 // Wait for reconnection
+//                 if (WiFi.status() == WL_CONNECTED)
+//                     state = CONNECTING_MQTT;
+//             break;
 
-            case CONNECTING_MQTT:
-                // Turn on LED to indicate disconnected
-                digitalWrite(LED_PIN, HIGH);
+//             case CONNECTING_MQTT:
+//                 // Turn on LED to indicate disconnected
+//                 digitalWrite(LED_PIN, HIGH);
 
-                connectMqtt();
+//                 connectMqtt();
 
-                // Update MQTT server of current shade position upon reconnection
-                // sendMqtt((String) motorCurrentPercentage());
+//                 // Update MQTT server of current shade position upon reconnection
+//                 // sendMqtt((String) motorCurrentPercentage());
 
-                // Turn off LED to indicate fully connected
-                digitalWrite(LED_PIN, LOW);
-                state = READ_MQTT_MSG;
-            break;
+//                 // Turn off LED to indicate fully connected
+//                 digitalWrite(LED_PIN, LOW);
+//                 state = READ_MQTT_MSG;
+//             break;
 
-            case READ_MQTT_MSG:
-                // Check wifi connection
-                if (WiFi.status() == WL_NO_SSID_AVAIL)
-                    state = RECONNECTING_WIFI;
+//             case READ_MQTT_MSG:
+//                 // Check wifi connection
+//                 if (WiFi.status() == WL_NO_SSID_AVAIL)
+//                     state = RECONNECTING_WIFI;
 
-                // Check mqtt connection
-                if (!mqttClient.connected())
-                    state = CONNECTING_MQTT;
+//                 // Check mqtt connection
+//                 if (!mqttClient.connected())
+//                     state = CONNECTING_MQTT;
         
-                // Use non blocking method to check for messages
-                mqttClient.loop();
-            break;
-        }
-        delay(100);
+//                 // Use non blocking method to check for messages
+//                 mqttClient.loop();
+//             break;
+//         }
+//         delay(100);
 
-        #if __has_include("ota.h")
-        ArduinoOTA.handle();
-        #endif
-    }
-}
-
-
-// TODO: Add timeout and restart, watchdog timer
-void startWifi() {
-    LOGI("Attempting to connect to WPA SSID: %s", ssid.c_str());
-
-    while (WiFi.begin(ssid.c_str(), pass.c_str()) != WL_CONNECTED)
-        delay(5000);
-
-    state = CONNECTING_MQTT;
-
-    LOGI("Connected to the WiFi, IP: %s", WiFi.localIP().toString().c_str());
-}
+//         #if __has_include("ota.h")
+//         ArduinoOTA.handle();
+//         #endif
+//     }
+// }
 
 
-void connectWifi(WiFiEvent_t event, WiFiEventInfo_t info) {
-    LOGI("Disconnected to WiFi");
+// // TODO: Add timeout and restart, watchdog timer
+// void startWifi() {
+//     LOGI("Attempting to connect to WPA SSID: %s", ssid.c_str());
 
-    WiFi.begin(ssid.c_str(), pass.c_str());
+//     while (WiFi.begin(ssid.c_str(), pass.c_str()) != WL_CONNECTED)
+//         delay(5000);
 
-    LOGI("Reconnected to WiFi");
-}
+//     state = CONNECTING_MQTT;
 
-
-void readMqtt(char* topic, byte* buf, unsigned int len) {
-    String message = "";
-    for (int i = 0; i < len; i++) message += (char) buf[i];
-    int command = message.toInt();
-
-    LOGI("Received message: %s", message);
-
-    if (command >= 0) motor.move(command);
-    else if (command == COVER_STOP) motor.stop();
-    else if (command == COVER_OPEN) motor.min();
-    else if (command == COVER_CLOSE) motor.max();
-    else if (command == COVER_SET_MAX) motor.setMax();
-    else if (command == COVER_SET_MIN) motor.setMin();
-    else if (command == SYS_RESET) motor.resetSettings();
-    else if (command == SYS_REBOOT) ESP.restart();
-}
+//     LOGI("Connected to the WiFi, IP: %s", WiFi.localIP().toString().c_str());
+// }
 
 
-// TODO: add timeout and restart, watchdog timer
-void connectMqtt() {
-    LOGI("Attempting to connect to MQTT broker: %s", brokerIP.c_str());
+// void connectWifi(WiFiEvent_t event, WiFiEventInfo_t info) {
+//     LOGI("Disconnected to WiFi");
 
-    mqttClient.setServer(brokerIP.c_str(), brokerPort);
+//     WiFi.begin(ssid.c_str(), pass.c_str());
+
+//     LOGI("Reconnected to WiFi");
+// }
+
+
+// void readMqtt(char* topic, byte* buf, unsigned int len) {
+//     String message = "";
+//     for (int i = 0; i < len; i++) message += (char) buf[i];
+//     int command = message.toInt();
+
+//     LOGI("Received message: %s", message);
+
+//     if (command >= 0) motor_task.move(command);
+//     else if (command == COVER_STOP) motor_task.stop();
+//     else if (command == COVER_OPEN) motor_task.min();
+//     else if (command == COVER_CLOSE) motor_task.max();
+//     else if (command == COVER_SET_MAX) motor_task.setMax();
+//     else if (command == COVER_SET_MIN) motor_task.setMin();
+//     else if (command == SYS_RESET) motor_task.resetSettings();
+//     else if (command == SYS_REBOOT) ESP.restart();
+// }
+
+
+// // TODO: add timeout and restart, watchdog timer
+// void connectMqtt() {
+//     LOGI("Attempting to connect to MQTT broker: %s", brokerIP.c_str());
+
+//     mqttClient.setServer(brokerIP.c_str(), brokerPort);
     
-    while (!mqttClient.connect(mqttID.c_str(), mqttUser.c_str(), mqttPass.c_str()));
+//     while (!mqttClient.connect(mqttID.c_str(), mqttUser.c_str(), mqttPass.c_str()));
 
-    mqttClient.subscribe(inTopic.c_str());
-    mqttClient.setCallback(readMqtt);
+//     mqttClient.subscribe(inTopic.c_str());
+//     mqttClient.setCallback(readMqtt);
 
-    LOGI("Connected to the MQTT broker, topic: %s", inTopic.c_str());
-}
+//     LOGI("Connected to the MQTT broker, topic: %s", inTopic.c_str());
+// }
 
 
-void sendMqtt(String message) {
-    mqttClient.publish(secretOutTopic.c_str(), message.c_str());
-    LOGI("Sent message: %s", message);
-}
+// void sendMqtt(String message) {
+//     mqttClient.publish(secretOutTopic.c_str(), message.c_str());
+//     LOGI("Sent message: %s", message);
+// }
