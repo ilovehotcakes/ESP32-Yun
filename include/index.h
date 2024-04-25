9f520916ed0bf81ff281d8354b5de5c20be1ccfb
Jason Chen <kidultimate@gmail.com>
2024-04-25 09:54:26 -0700

    Added error handling and console log for events

--- include/index.h
+++ include/index.h
@@ -77,33 +77,42 @@ p {
         <input id="percentage-slider" class="slider" type="range" min="0" max="100" step="1">
     </div>
     <script>
-window.addEventListener('load', function() {
-    const websocket = new WebSocket(`ws://${window.location.hostname}/ws`);
-    let percentage_slider_ = document.getElementById('percentage-slider');
+window.addEventListener('load', () => {
+    try {
+        const websocket = new WebSocket(`ws://${window.location.hostname}/ws`);
+        const percentage_slider_ = document.getElementById('percentage-slider');
 
-    websocket.onopen = function(event) {
-        console.log('Connection established');
+        websocket.onopen = (event) => {
+            websocket.send('-100');  // To get initial motor position
+            document.getElementById('stop-button').addEventListener('click', () => { websocket.send('-1'); });
+            document.getElementById('open-button').addEventListener('click', () => { websocket.send('0'); });
+            document.getElementById('close-button').addEventListener('click', () => { websocket.send('100'); });
+            percentage_slider_.addEventListener('change', () => { websocket.send(percentage_slider_.value); });
+            console.log(`Connection established with ${window.location.hostname}`);
+            console.log(event)
+        };
 
-        document.getElementById('stop-button').addEventListener('click', function() { websocket.send('-1'); });
-        document.getElementById('open-button').addEventListener('click', function() { websocket.send('0'); });
-        document.getElementById('close-button').addEventListener('click', function() { websocket.send('100'); });
-        percentage_slider_.addEventListener('change', function() { websocket.send(percentage_slider_.value); });
+        websocket.onclose = (event) => {
+            console.log(`Connection with ${window.location.hostname} closed`);
+            console.log(event)
+        };
 
-        websocket.send('-100');  // To get initial motor position
-    };
-    websocket.onclose = function(event) {
-        console.log('Connection closed');
-    };
-    websocket.onerror = function(error) {
-        console.log('error');
-    };
-    websocket.onmessage = function(event) {
-        var data = event.data
-        console.log(data);
-        if (data > -1) {
-            percentage_slider_.value = data;
-        }
-    };
+        websocket.onerror = (error) => {
+            console.log("Websocket error");
+            console.log(error)
+        };
+
+        websocket.onmessage = (event) => {
+            const data = event.data
+            console.log(data);
+            if (data > -1) {
+                percentage_slider_.value = data;
+            }
+        };
+    } catch (error) {
+        console.log("Failed to connect to websocket")
+        console.log(error)
+    }
 });
     </script>
 </body>
