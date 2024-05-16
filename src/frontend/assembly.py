71881ac7d9c5644c888137e3a3a8af48db2b8c39
Jason Chen <kidultimate@gmail.com>
2024-05-16 15:58:12 -0700

    automatically encode png to base64; added link to css and js

--- src/frontend/assembly.py
+++ src/frontend/assembly.py
@@ -1,15 +1,30 @@
+import base64
+
 def process_html():
+    current_directory = "./src/frontend"
     assembled_html = "const char index_html[] = R\"rawliteral("
     with open("./src/frontend/index.html", 'r') as html_file:
         for html_line in html_file:
-            if html_line == "        /* index.css */\n":
-                with open("./src/frontend/index.css", 'r') as css_file:
+            if "index.css" in html_line:
+                file_path = current_directory + html_line[html_line.index("href=")+7:-3]
+                assembled_html += "\t<style>\n"
+                with open(file_path, 'r') as css_file:
                     for line in css_file:
                         assembled_html += line
-            elif html_line == "        /* index.js */\n":
-                with open("./src/frontend/index.js", 'r') as js_file:
-                    for line in js_file:
+                assembled_html += "\t</style>\n"
+            elif "index.js" in html_line:
+                file_path = current_directory + html_line[18:27]
+                assembled_html += "\t<script>\n"
+                with open(file_path, 'r') as css_file:
+                    for line in css_file:
                         assembled_html += line
+                assembled_html += "\t</script>\n"
+            elif ".png" in html_line:
+                file_path = current_directory + html_line[html_line.index("href=")+7:-5]
+                with open(file_path, 'rb') as img:
+                    b64_string = base64.b64encode(img.read()).decode("utf-8")
+                    href_tag = html_line[:html_line.index("href=") + 6] + "data:image/png;base64," + b64_string + "\" />\n"
+                    assembled_html += href_tag
             else:
                 assembled_html += html_line
 
